7b5dc1e5852175c8dee632fff2e36559ac61b986
Maheshmelapakula <mahesh250321@gmail.com>
2025-01-22 11:32:49 +0530

    connect

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -12,7 +12,7 @@ const Register = () => {
   const handleFormSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("http://localhost:2000/api/users/register", {
+      const response = await axios.post("/api/users/register", {
         name,
         email,
         password,
